f6e4774a7f22c20bfd606a9662e4e0c56462b163
BarjinderGoyal <111654345+BarjinderGoyal@users.noreply.github.com>
2022-08-27 23:23:28 +0530

    Delete ASSIGNEMENT-2 directory

--- ASSIGNEMENT-2/Triangle.js
+++
@@ -1,9 +0,0 @@
-let triangle=(a,b,c)=>{
-    if(a===b &&b===c){
-        return 'Equilateral triangle'
-    }else if(a!=b && a!=c && b!=c){
-        return 'Scalene triangle'
-    }else{
-        return 'Isosceles triangle'
-    }
-}
